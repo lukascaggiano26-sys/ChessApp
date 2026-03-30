a8e2d2b2cffd25aff9a8f42dc66bd6bccdf8d8e1
lukascaggiano26-sys <lukas_caggiano26@milton.edu>
2026-03-30 10:46:10 -0400

    Simplify style attributes in EvaluationBar component

--- src/components/chess/game/EvaluationBar.tsx
+++ src/components/chess/game/EvaluationBar.tsx
@@ -44,9 +44,9 @@ export const EvaluationBar = ({
         >
           <div
             className="evalbar-black"
-            style={{ height: `${100 - fillPercent}%`, width: `${100 - fillPercent}%` }}
+            style={{ height: `${100 - fillPercent}%` }}
           />
-          <div className="evalbar-white" style={{ height: `${fillPercent}%`, width: `${fillPercent}%` }} />
+          <div className="evalbar-white" style={{ height: `${fillPercent}%` }} />
         </div>
       </div>
 
